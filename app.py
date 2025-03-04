8589280692eefd8eb41d39101ab7977f04da254f
nawrec86 <kilinc.metin266@gmail.com>
2025-03-05 01:32:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def home():
     return jsonify({"message": "Welcome to School Management System API"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
-
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
